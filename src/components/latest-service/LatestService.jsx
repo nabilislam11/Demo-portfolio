63bec617b55ba7528acb89267b3afe0dfdadff19
nabilislam11 <hamjanabil11@gmail.com>
2026-03-10 16:59:16 +0600

    skill area complete

--- src/components/latest-service/LatestService.jsx
+++ src/components/latest-service/LatestService.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import service from "../../assets/service.png"
 const LatestService = () => {
     return (
-        <div className=' container'>
+        <div className=' container px-[15px] pt-[120px] '>
             <div className="flex flex-col gap-8">
                 <div className="text-center  flex flex-col items-center justify-between  gap-1  ">
                     <p className='font-rubik  font-medium  text-[16px] leading-[28px] text-[#FF494A] '>Latest Service</p>
@@ -11,35 +11,35 @@ const LatestService = () => {
                         them improve their performance, efficiency, and organizational</p>
                 </div>
                 <div className="flex justify-between items-center ">
-                    <div className=" w-[50%] mt-[32px] px-[15px] flex flex-col  justify-between items-center gap-5 ">
-                     
-                     <div className="  py-[34px] px-[38px] bg-[#F4F4F4] rounded-[20px]  flex flex-col gap-5.5 ">
+                    <div className=" w-[50%] mt-[32px]  flex flex-col  justify-between items-center gap-5 ">
+
+                        <div className="  py-[34px] px-[38px] bg-[#F4F4F4] rounded-[20px]  flex flex-col gap-5.5 ">
                             <div className="flex items-center  gap-3.5 ">
                                 <p>01.</p>
                                 <p className='font-bold font-rajdhani text-[24px] leading-[30px]  '>A Portfolio of Creativity</p>
                             </div>
                             <p className='font-normal  font-rubik  text-[16px] leading-[26px] '>Business consulting consultants provide expert advice and guida the a
-businesses to help theme their performance efficiency</p>
+                                businesses to help theme their performance efficiency</p>
                         </div>
-                         <div className="  py-[34px] px-[38px] bg-[#F4F4F4] rounded-[20px]  flex flex-col gap-5.5 ">
+                        <div className="  py-[34px] px-[38px] bg-[#F4F4F4] rounded-[20px]  flex flex-col gap-5.5 ">
                             <div className="flex items-center  gap-3.5 ">
                                 <p>01.</p>
                                 <p className='font-bold font-rajdhani text-[24px] leading-[30px]  '>A Portfolio of Creativity</p>
                             </div>
                             <p className='font-normal  font-rubik  text-[16px] leading-[26px] '>Business consulting consultants provide expert advice and guida the a
-businesses to help theme their performance efficiency</p>
+                                businesses to help theme their performance efficiency</p>
                         </div>
-                         <div className="  py-[34px] px-[38px] bg-[#F4F4F4] rounded-[20px]  flex flex-col gap-5.5 ">
+                        <div className="  py-[34px] px-[38px] bg-[#F4F4F4] rounded-[20px]  flex flex-col gap-5.5 ">
                             <div className="flex items-center  gap-3.5 ">
                                 <p>01.</p>
                                 <p className='font-bold font-rajdhani text-[24px] leading-[30px]  '>A Portfolio of Creativity</p>
                             </div>
                             <p className='font-normal  font-rubik  text-[16px] leading-[26px] '>Business consulting consultants provide expert advice and guida the a
-businesses to help theme their performance efficiency</p>
+                                businesses to help theme their performance efficiency</p>
                         </div>
 
                     </div>
-                    <div className=" w-[50%] mt-[32px] pl-[85px] pr-[15px] ">
+                    <div className=" w-[50%] mt-[32px] pl-[85px]  ">
                         <img src={service} alt="" />
                     </div>
                 </div>
--- src/components/skill-area/SkillArea.jsx
+++ src/components/skill-area/SkillArea.jsx
@@ -2,23 +2,102 @@ import React from 'react'
 
 const SkillArea = () => {
     return (
-        <div className='container  '>
+        <div className='container px-[15px] pt-[90px] '>
             <div className="grid grid-cols-1 md:grid-cols-2 gap-7.5 ">
-                <div className="mt-7.5 flex flex-col gap-7.5 items-center ">
+                <div className=" mt-7.5 flex flex-col gap-7.5  ">
                     <h1 className='font-bold font-rajdhani text-[32px] leading-[42px] '>Design Skill </h1>
-                    <div className="">
-                        <div className="flex items-center justify-between">
+                    <div className=" flex flex-col gap-4 ">
+                        <div className="flex items-center justify-between ">
                             <p className='font-normal  font-rubik text-[14px] leading-[17.5px] '>PHOTOSHOT</p>
                             <p className='font-normal  font-rubik text-[14px] leading-[24.5px] '>100%</p>
                         </div>
-                        <div className="w-full h-[1.5px] bg-[#F4F4F4] rounded-[10px] overflow-hidden  ">
+                        <div className="w-full h-1.5 bg-[#F4F4F4] rounded-[10px] overflow-hidden  ">
                             <div className="bg-red-500 w-full h-full transform-all duration-1000  ease-out "></div>
                         </div>
-                     
+
+
+                    </div>
+                    <div className=" flex flex-col gap-4 ">
+                        <div className="flex items-center justify-between ">
+                            <p className='font-normal  font-rubik text-[14px] leading-[17.5px] '>FIGMA</p>
+                            <p className='font-normal  font-rubik text-[14px] leading-[24.5px] '>95%</p>
+                        </div>
+                        <div className="w-full h-1.5 bg-[#F4F4F4] rounded-[10px] overflow-hidden  ">
+                            <div className="bg-red-500 w-[95%] h-full transform-all duration-1000  ease-out "></div>
+                        </div>
+
+
+                    </div>
+                    <div className=" flex flex-col gap-4 ">
+                        <div className="flex items-center justify-between ">
+                            <span className='font-normal  font-rubik text-[14px] leading-[17.5px] '>ADOBE XD</span>
+                            <span className='font-normal  font-rubik text-[14px] leading-[24.5px] '>60%</span>
+                        </div>
+                        <div className="w-full h-1.5 bg-[#F4F4F4] rounded-[10px] overflow-hidden  ">
+                            <div className="bg-red-500 w-[60%] h-full transform-all duration-1000  ease-out "></div>
+                        </div>
+
+
+                    </div>
+                    <div className=" flex flex-col gap-4 ">
+                        <div className="flex items-center justify-between ">
+                            <span className='font-normal  font-rubik text-[14px] leading-[17.5px] '>ADOBE ILLUSTRATOR</span>
+                            <span className='font-normal  font-rubik text-[14px] leading-[24.5px] '>70%</span>
+                        </div>
+                        <div className="w-full h-1.5 bg-[#F4F4F4] rounded-[10px] overflow-hidden  ">
+                            <div className="bg-red-500 w-[70%] h-full transform-all duration-1000  ease-out "></div>
+                        </div>
+
 
                     </div>
                 </div>
-                <div className=""></div>
+                <div className=" mt-7.5 flex flex-col gap-7.5  ">
+                    <h1 className='font-bold font-rajdhani text-[32px] leading-[42px] '>Development Skill  </h1>
+                    <div className=" flex flex-col gap-4 ">
+                        <div className="flex items-center justify-between ">
+                            <p className='font-normal  font-rubik text-[14px] leading-[17.5px] '>PHOTOSHOT</p>
+                            <p className='font-normal  font-rubik text-[14px] leading-[24.5px] '>100%</p>
+                        </div>
+                        <div className="w-full h-1.5 bg-[#F4F4F4] rounded-[10px] overflow-hidden  ">
+                            <div className="bg-red-500 w-full h-full transform-all duration-1000  ease-out "></div>
+                        </div>
+
+
+                    </div>
+                    <div className=" flex flex-col gap-4 ">
+                        <div className="flex items-center justify-between ">
+                            <p className='font-normal  font-rubik text-[14px] leading-[17.5px] '>FIGMA</p>
+                            <p className='font-normal  font-rubik text-[14px] leading-[24.5px] '>95%</p>
+                        </div>
+                        <div className="w-full h-1.5 bg-[#F4F4F4] rounded-[10px] overflow-hidden  ">
+                            <div className="bg-red-500 w-[95%] h-full transform-all duration-1000  ease-out "></div>
+                        </div>
+
+
+                    </div>
+                    <div className=" flex flex-col gap-4 ">
+                        <div className="flex items-center justify-between ">
+                            <span className='font-normal  font-rubik text-[14px] leading-[17.5px] '>ADOBE XD</span>
+                            <span className='font-normal  font-rubik text-[14px] leading-[24.5px] '>60%</span>
+                        </div>
+                        <div className="w-full h-1.5 bg-[#F4F4F4] rounded-[10px] overflow-hidden  ">
+                            <div className="bg-red-500 w-[60%] h-full transform-all duration-1000  ease-out "></div>
+                        </div>
+
+
+                    </div>
+                    <div className=" flex flex-col gap-4 ">
+                        <div className="flex items-center justify-between ">
+                            <span className='font-normal  font-rubik text-[14px] leading-[17.5px] '>ADOBE ILLUSTRATOR</span>
+                            <span className='font-normal  font-rubik text-[14px] leading-[24.5px] '>70%</span>
+                        </div>
+                        <div className="w-full h-1.5 bg-[#F4F4F4] rounded-[10px] overflow-hidden  ">
+                            <div className="bg-red-500 w-[70%] h-full transform-all duration-1000  ease-out "></div>
+                        </div>
+
+
+                    </div>
+                </div>
             </div>
         </div>
     )
