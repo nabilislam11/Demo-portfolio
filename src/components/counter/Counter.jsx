ad1d80530690dadf019c4b68e463c47b0aa6d361
nabilislam11 <hamjanabil11@gmail.com>
2026-03-18 17:18:45 +0600

    counter complete

--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -17,39 +17,39 @@ const Counter = () => {
                 </div>
                 <div className="w-[50%] grid grid-cols-1 md:grid-cols-2 gap-8  mt-[32px]  ml-[17px]  ">
                     <div className="py-[35px] px-[17px]  border border-b-[#FF494A] border-t-[#141414] border-l-[#141414] border-r-[#FF494A] bg-[#F4F4F4] dark:bg-[#141414] shadow-[2px_2px_0px_0px_#FF494A] space-y-1  rounded-[20px]  text-center flex flex-col justify-between items-center ">
-                        <div className="flex  ">
-                            <p className='font-rajdhani  font-bold  text-[48px] leading-[60px] text-black dark:text-white'> 10</p>
-                            <p className='font-rajdhani  font-bold  text-[48px] leading-[60px] text-black dark:text-[#9F9F9F]'> K+</p>
+                        <div className="flex  text-black dark:text-white ">
+                            <p className='font-rajdhani  font-bold  text-[48px] leading-[60px]'> 10</p>
+                            <p className='font-rajdhani  font-bold  text-[48px] leading-[60px]'> K+</p>
 
                         </div>
-                        <p className='font-rubik  font-normal text-[16px] leading-[30px] text-[#5D6570] '>Our Natural Products</p>
+                        <p className='font-rubik  font-normal text-[16px] leading-[30px] text-[#5D6570] dark:text-[#9F9F9F] '>Our Natural Products</p>
 
                     </div>
-                    <div className="py-[30px] px-[15px] bg-[#F4F4F4]  space-y-1  rounded-[20px]  text-center flex flex-col  justify-between items-center   border border-b-[#FF494A] border-t-[#F4F4F4] border-l-[#F4F4F4] border-r-[#FF494A] bg-[#F4F4F4] shadow-[2px_2px_0px_0px_#FF494A] bg-[#F4F4F4]">
-                        <div className="flex ">
+                    <div className="py-[30px] px-[15px] bg-[#F4F4F4]  space-y-1  rounded-[20px]  text-center flex flex-col  justify-between items-center  dark:bg-[#141414]  border border-b-[#FF494A] border-t-[#F4F4F4] border-l-[#F4F4F4] border-r-[#FF494A] bg-[#F4F4F4] shadow-[2px_2px_0px_0px_#FF494A] bg-[#F4F4F4]">
+                        <div className="flex text-black dark:text-white ">
                             <p className='font-rajdhani  font-bold  text-[48px] leading-[60px]'> 20</p>
                             <p className='font-rajdhani  font-bold  text-[48px] leading-[60px]'> K+</p>
 
                         </div>
-                        <p className='font-rubik  font-normal text-[16px] leading-[30px] text-[#5D6570] '>Our Project Complete</p>
+                        <p className='font-rubik  font-normal text-[16px] leading-[30px] text-[#5D6570] dark:text-[#9F9F9F]'>Our Project Complete</p>
 
                     </div>
                     <div className="py-[30px] px-[15px] bg-[#F4F4F4]  space-y-1  rounded-[20px]  text-center flex flex-col  justify-between items-center   border border-b-[#FF494A]  border-t-[#141414] border-l-[#141414] border-r-[#FF494A] bg-[#F4F4F4] dark:bg-[#141414] shadow-[2px_2px_0px_0px_#FF494A] bg-[#F4F4F4] ">
-                        <div className="flex ">
-                            <p className='font-rajdhani  font-bold  text-[48px] leading-[60px] text-black dark:text-white'> 10</p>
+                        <div className="flex text-black dark:text-white ">
+                            <p className='font-rajdhani  font-bold  text-[48px] leading-[60px]'> 10</p>
                             <p className='font-rajdhani  font-bold  text-[48px] leading-[60px]'> K+</p>
 
                         </div>
                         <p className='font-rubik  font-normal text-[16px] leading-[30px] text-[#5D6570] '>Our Natural Products</p>
 
                     </div>
-                    <div className="py-[30px] px-[15px] bg-[#F4F4F4]  space-y-1  rounded-[20px]  text-center flex flex-col  justify-between items-center  border border-b-[#FF494A] border-t-[#F4F4F4] border-l-[#F4F4F4] border-r-[#FF494A] bg-[#F4F4F4] shadow-[2px_2px_0px_0px_#FF494A] bg-[#F4F4F4] ">
-                        <div className="flex ">
+                    <div className="py-[30px] px-[15px] bg-[#F4F4F4]  dark:bg-[#141414] space-y-1  rounded-[20px]  text-center flex flex-col  justify-between items-center  border border-b-[#FF494A] border-t-[#F4F4F4] border-l-[#F4F4F4] border-r-[#FF494A] bg-[#F4F4F4] shadow-[2px_2px_0px_0px_#FF494A] bg-[#F4F4F4] ">
+                        <div className="flex text-black dark:text-white ">
                             <p className='font-rajdhani  font-bold  text-[48px] leading-[60px]'> 20</p>
                             <p className='font-rajdhani  font-bold  text-[48px] leading-[60px]'> K+</p>
 
                         </div>
-                        <p className='font-rubik  font-normal text-[16px] leading-[30px] text-[#5D6570] '>Our Project Complete</p>
+                        <p className='font-rubik  font-normal text-[16px] leading-[30px] text-[#5D6570] dark:text-[#9F9F9F]'>Our Project Complete</p>
 
                     </div>
                 </div>
