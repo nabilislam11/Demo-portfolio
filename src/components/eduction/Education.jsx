9ce964860a8f61372e332f95c905239e78573177
nabilislam11 <hamjanabil11@gmail.com>
2026-04-04 03:49:41 +0600

    perfection

--- src/components/eduction/Education.jsx
+++ src/components/eduction/Education.jsx
@@ -7,8 +7,7 @@ const Education = () => {
                 <h2 className=' font-bold font-rajdhani text-[32px] text-[#141414] dark:text-white leading-[42px] '>Education</h2>
                 <div className=" grid grid-cols-1 md:grid-cols-2  gap-8">
                     <div className="  mt-[32px]  flex flex-col  justify-between items-center gap-5 ">
-
-                        <div className="  pt-[48px] pb-[50px] px-[38px] bg-[#F4F4F4] dark:bg-[#141414]   rounded-[20px]  flex flex-col gap-1.5 ">
+                        <div className="pt-[48px] pb-[50px] px-[38px] bg-[#F4F4F4] dark:bg-[#141414] rounded-[20px] flex flex-col gap-1.5 ">
 
                             <p className='font-normal  font-rubik  text-[16px] leading-[26px] text-[#141414] dark:text-[#FFFFFF] '>Trainer Marketing</p>
                             <p className='font-bold   font-rajdhani   text-[18px] leading-[28px] text-[#141414] dark:text-[#FFFFFF] '>2005-2009</p>
@@ -16,47 +15,35 @@ const Education = () => {
                             <p className='font-normal  font-rubik  text-[16px] leading-[30px] text-[#5D6570] dark:text-[#9F9F9F] '>A personal portfolio is a curated collection of an individual's professional
                                 work, showcasing their skills, experience A personal portfolio.</p>
                         </div>
-
-
                     </div>
-                    <div className="  mt-[32px]  flex flex-col  justify-between items-center gap-5 ">
+                    <div className="  mt-[32px]  flex flex-col justify-between items-center gap-5 ">
+                        <div className="pt-[48px] pb-[50px] px-[38px] bg-[#F4F4F4] dark:bg-[#141414] rounded-[20px] flex flex-col gap-1.5  ">
 
-                        <div className="  pt-[48px] pb-[50px] px-[38px] bg-[#F4F4F4] dark:bg-[#141414]  rounded-[20px]  flex flex-col gap-1.5  ">
+                            <p clzassName='font-normal  font-rubik text-[16px] leading-[26px] text-[#141414] dark:text-[#FFFFFF]'>Trainer Marketing</p>
+                            <p className='font-bold font-rajdhani text-[18px] leading-[28px] text-[#141414]  dark:text-[#FFFFFF]'>2005-2009</p>
 
-                            <p clzassName='font-normal  font-rubik  text-[16px] leading-[26px] text-[#141414] dark:text-[#FFFFFF] '>Trainer Marketing</p>
-                            <p className='font-bold   font-rajdhani   text-[18px] leading-[28px] text-[#141414]  dark:text-[#FFFFFF] '>2005-2009</p>
-
-                            <p className='font-normal  font-rubik  text-[16px] leading-[30px] text-[#5D6570] dark:text-[#9F9F9F]  '>A personal portfolio is a curated collection of an individual's professional
+                            <p className='font-normal font-rubik  text-[16px] leading-[30px] text-[#5D6570] dark:text-[#9F9F9F]'>A personal portfolio is a curated collection of an individual's professional
                                 work, showcasing their skills, experience A personal portfolio.</p>
                         </div>
-
-
                     </div>
-                    <div className="  mt-[32px]  flex flex-col  justify-between items-center gap-5 ">
+                    <div className="mt-[32px] flex flex-col  justify-between items-center gap-5 ">
 
-                        <div className="  pt-[48px] pb-[50px] px-[38px] bg-[#F4F4F4] dark:bg-[#141414]  rounded-[20px]  flex flex-col gap-1.5 ">
+                        <div className="pt-[48px] pb-[50px] px-[38px] bg-[#F4F4F4] dark:bg-[#141414]  rounded-[20px]  flex flex-col gap-1.5 ">
 
                             <p className='font-normal  font-rubik  text-[16px] leading-[26px] text-[#141414] dark:text-[#FFFFFF]  '>Trainer Marketing</p>
-                            <p className='font-bold   font-rajdhani   text-[18px] leading-[28px] text-[#141414] dark:text-[#FFFFFF]'>2005-2009</p>
+                            <p className='font-bold font-rajdhani   text-[18px] leading-[28px] text-[#141414] dark:text-[#FFFFFF]'>2005-2009</p>
 
-                            <p className='font-normal  font-rubik  text-[16px] leading-[30px] text-[#5D6570] dark:text-[#9F9F9F]'>A personal portfolio is a curated collection of an individual's professional
+                            <p className='font-normal font-rubik  text-[16px] leading-[30px] text-[#5D6570] dark:text-[#9F9F9F]'>A personal portfolio is a curated collection of an individual's professional
                                 work, showcasing their skills, experience A personal portfolio.</p>
                         </div>
-
-
                     </div>
-                    <div className="  mt-[32px]  flex flex-col  justify-between items-center gap-5 ">
-
-                        <div className="  pt-[48px] pb-[50px] px-[38px] bg-[#F4F4F4] dark:bg-[#141414]  rounded-[20px] flex flex-col gap-1.5 ">
-
-                            <p className='font-normal  font-rubik  text-[16px] leading-[26px] text-[#141414] dark:text-[#FFFFFF] '>Trainer Marketing</p>
-                            <p className='font-bold   font-rajdhani text-[18px] leading-[28px] text-[#141414] dark:text-[#FFFFFF]'>2005-2009</p>
-
-                            <p className='font-normal  font-rubik  text-[16px] leading-[30px] text-[#5D6570] dark:text-[#9F9F9F]'>A personal portfolio is a curated collection of an individual's professional
+                    <div className=" mt-[32px]  flex flex-col justify-between items-center gap-5 ">
+                        <div className="pt-[48px] pb-[50px] px-[38px] bg-[#F4F4F4] dark:bg-[#141414]  rounded-[20px] flex flex-col gap-1.5 ">
+                            <p className='font-normal font-rubik  text-[16px] leading-[26px] text-[#141414] dark:text-[#FFFFFF] '>Trainer Marketing</p>
+                            <p className='font-bold  font-rajdhani text-[18px] leading-[28px] text-[#141414] dark:text-[#FFFFFF]'>2005-2009</p>
+                            <p className='font-normal font-rubik  text-[16px] leading-[30px] text-[#5D6570] dark:text-[#9F9F9F]'>A personal portfolio is a curated collection of an individual's professional
                                 work, showcasing their skills, experience A personal portfolio.</p>
                         </div>
-
-
                     </div>
                 </div>
             </div>
